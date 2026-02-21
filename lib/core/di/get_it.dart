f6cfef698ef40bc9c25a5e99d671a3cbc1a87b7a
JeremyPerezSwift <jeremyperez34000@gmail.com>
2026-02-21 09:38:27 +0100

    feat(di): implement dependency injection with GetIt
    - Register ApiClient as singleton
    - Register AuthRemoteDatasource with injected Dio instance
    - Register AuthRepository as singleton
    - Register UserBloc as factory
    - Centralize dependency setup in setup()

---
+++ lib/core/di/get_it.dart
@@ -0,0 +1,33 @@
+import 'package:chronicle_app/core/api/api_client.dart';
+import 'package:chronicle_app/features/auth/data/datasource/auth_remote_datasource.dart';
+import 'package:chronicle_app/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:chronicle_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:chronicle_app/features/auth/presentation/bloc/user_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+final GetIt getIt = GetIt.instance;
+
+void setup() {
+  registerApiClient();
+  registerDatasource();
+  registerRepository();
+  registerBloc();
+}
+
+void registerApiClient() {
+  getIt.registerSingleton(ApiClient());
+}
+
+void registerDatasource() {
+  var dio = getIt<ApiClient>().getDio();
+
+  getIt.registerSingleton(AuthRemoteDatasource(dio: dio));
+}
+
+void registerRepository() {
+  getIt.registerSingleton<AuthRepository>(AuthRepositoryImpl(authRemoteDatasource: getIt()));
+}
+
+void registerBloc() {
+  getIt.registerFactory(() => UserBloc(authRepository: getIt()));
+}
