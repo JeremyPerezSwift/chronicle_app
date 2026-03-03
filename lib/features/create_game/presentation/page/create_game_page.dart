c0ecbef2c3da6567ec4d2bc290dcf6ac059f7d75
JeremyPerezSwift <jeremyperez34000@gmail.com>
2026-03-03 17:23:48 +0100

    git add . git commit -m "feat(create_game): add CreateGame body UI and implement reusable NumberPicker widget"

--- lib/features/create_game/presentation/page/create_game_page.dart
+++ lib/features/create_game/presentation/page/create_game_page.dart
@@ -1,3 +1,5 @@
+import 'package:chronicle_app/core/ui/widgets/default_text_field.dart';
+import 'package:chronicle_app/features/create_game/presentation/widgets/number_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,6 +17,7 @@ class _CreateGamePageState extends State<CreateGamePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
+      body: _buildBody(context),
     );
   }
 
@@ -37,6 +40,40 @@ class _CreateGamePageState extends State<CreateGamePage> {
       ),
       titleSpacing: 0,
       toolbarHeight: 60,
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Title', style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500)),
+              const SizedBox(height: 10),
+              DefaultTextField(
+                hintText: 'Enter story title',
+                borderRadius: BorderRadius.circular(12),
+              ),
+              const SizedBox(height: 10),
+              Text('Rounds', style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500)),
+            ],
+          ),
+        ),
+
+        // ✅ donne une hauteur
+        SizedBox(
+          height: 52, // ajuste selon ton design
+          child: NumberPicker(
+            from: 3,
+            to: 10,
+            onNumberChanged: (value) {},
+          ),
+        ),
+      ],
     );
   }
 }
